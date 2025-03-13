f998254de68bc2b4cfb2f0423eda050bf028001b
a Joshua Tree <133618057+Ayohinliveaid@users.noreply.github.com>
2025-03-13 08:36:12 +0800

    realty api

---
+++ src/components/loginModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="windowStyle" v-if="props.showLoginModal">
+    <form action="" class="formStyle">
+      <label>phone number</label>
+      <input type="text" placeholder="Enter your phone number" v-model="form.pn" />
+      <label>varification code</label>
+      <input type="text" placeholder="Enter varification code" v-model="form.pw" />
+      <label>password</label>
+      <input type="text" placeholder="Enter your password" />
+      <button type="button" style="grid-column-start: span 2;" v-on:click="login">登录</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from "vue";
+const props = defineProps({
+  showLoginModal: Boolean,
+});
+const signup = ref(false);
+
+const form =ref({
+  pn:'',
+  pw:''
+})
+const login = () => {
+  console.log(form.value);
+}
+</script>
+
+<style scoped>
+.windowStyle {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 300px;
+  box-sizing: border-box;
+  background-color: lightblue;
+  z-index: 100000;
+  padding: 10px;
+
+  .formStyle {
+    display: grid;
+    grid-template-columns: 20% 75%;
+    grid-template-rows: 20% 20% 20% 10%;
+    height: 100%;
+    gap:5%;
+    box-sizing: border-box;
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,60 +1,71 @@
 <template>
   <!-- <div class="layout" ref="layout"> -->
-    <div id="container"></div>
-    <div class="mapStyle" id="map">
-      <div class="setPitch">
-        <button
-          class="buttonStyle"
-          @mousedown="modifyAttribute('pitch', '+1')"
-          @mouseup="stopModifing"
-        >
-          +
-        </button>
-        <button
-          class="buttonStyle"
-          @mousedown="modifyAttribute('pitch', '-1')"
-          @mouseup="stopModifing"
-        >
-          -
-        </button>
-      </div>
-      <div class="setRotation">
-        <button
-          class="buttonStyle"
-          @mousedown="modifyAttribute('rotation', '+1')"
-          @mouseup="stopModifing"
-        >
-          +
-        </button>
-        <button
-          class="buttonStyle"
-          @mousedown="modifyAttribute('rotation', '-1')"
-          @mouseup="stopModifing"
-        >
-          -
-        </button>
-      </div>
+  <div id="container"></div>
+  <div class="mapStyle" id="map" @click="handleClick">
+    <div class="setPitch">
+      <button
+        class="buttonStyle"
+        @mousedown="modifyAttribute('pitch', '+1')"
+        @mouseup="stopModifing"
+      >
+        +
+      </button>
+      <button
+        class="buttonStyle"
+        @mousedown="modifyAttribute('pitch', '-1')"
+        @mouseup="stopModifing"
+      >
+        -
+      </button>
+    </div>
+    <div class="setRotation">
+      <button
+        class="buttonStyle"
+        @mousedown="modifyAttribute('rotation', '+1')"
+        @mouseup="stopModifing"
+      >
+        +
+      </button>
+      <button
+        class="buttonStyle"
+        @mousedown="modifyAttribute('rotation', '-1')"
+        @mouseup="stopModifing"
+      >
+        -
+      </button>
+    </div>
 
-      <div class="dialogStyle" ref="dialog">
-        <div class="outputStyle">
-          <div class="outputTextStyle titleStyle" ref="title">这是提出的问题</div>
-          <div class="outputTextStyle contentStyle">{{ outputText }}</div>
-          <div class="saveButtonStyle" v-if="outputText" @click="save">保存这个回答</div>
+    <div class="userStyle" ref="user" v-on:click="toggleShowLoginModal">Login</div>
+    <loginModal :showLoginModal="showLoginModal" />
 
-          <textarea
-            id="inputBox"
-            ref="inputBox"
-            v-model="inputText"
-            placeholder="给Estima发送消息"
-            placeholder-class="textarea-placeholder"
-            @input="adjustHeight"
-            class="inputStyle"
-            @keyup.enter.prevent="submit"
-            @keydown.enter.prevent
-          />
+    <div class="dialogSectioinStyle" ref="dialogSectioin">
+      <div class="dialogSetStyle" @wheel="handleScroll" ref="dialogSet">
+        <div class="dialogStyle" v-for="i in 3">
+          <div class="outputTextStyle titleStyle" ref="titleRefs">这是提出的问题</div>
+          <div v-if="!hide">
+            <div class="outputTextStyle contentStyle">{{ outputText }}</div>
+            <div class="saveButtonStyle" v-if="outputText" @click="save">
+              保存这个回答
+            </div>
+          </div>
         </div>
       </div>
+
+      <textarea
+        id="inputBox"
+        ref="inputBox"
+        v-model="inputText"
+        placeholder="给Estima发送消息"
+        placeholder-class="textarea-placeholder"
+        @input="adjustHeight"
+        class="inputStyle"
+        @keyup.enter.prevent="submit"
+        @keydown.enter.prevent
+        v-if="!hide"
+      />
     </div>
+  </div>
+
   <!-- </div> -->
 </template>
 
@@ -69,6 +80,8 @@ import {
   HeatmapLayer,
 } from "@antv/l7";
 import { Mapbox, GaodeMap } from "@antv/l7-maps";
+import { nodeListProps } from "ant-design-vue/es/vc-tree/props";
+import loginModal from "/src/components/loginModal.vue";
 
 // 初始化文本和文本框高度
 const text = ref("");
@@ -84,23 +97,104 @@ const adjustHeight = () => {
 
 //提交问题
 const inputText = ref("");
-const title = ref(null);
+//title引用的数组
+const titleRefs = ref([]);
+
 const outputText = ref(
   "获取对输入框的引用，把他的高度设置为滚动高度即可，但由于一次性删除内容后，scrollheight不会马上恢复到最初值，所以需要在之前加入，height为auto的设置，使的textarea根据内容调整height和scrollheight"
 );
 
 const submit = () => {
-  title.value.innerText = inputText.value;
+  titleRefs.value[titleRefs.value.length - 1].innerText = inputText.value;
   inputText.value = "";
   setTimeout(() => {
     //浏览器计算高度有延迟，需要异步执行该函数
     adjustHeight();
   }, 50);
+  //滚动到最右侧的
+  const width = parseFloat(window.getComputedStyle(dialogSet.value).width);
+  dialogSet.value.scrollLeft += titleRefs.value.length * width;
 };
 
 //保存回答
 const save = () => {};
 
+// 隐藏对话框回答部分
+const hide = ref(false);
+const dialogSectioin = ref(null);
+const handleClick = (event) => {
+  if (dialogSectioin.value.contains(event.target)) {
+    if (hide.value === false) return;
+    hide.value = false;
+    titleRefs.value.forEach((v, i) => {
+      v.style.borderRadius = "10px 10px 0 0";
+      v.style.opacity = "1";
+    });
+    dialogSet.value.style.marginBottom = "20px";
+    //设置向下舒展的动态效果
+    hide.value = false;
+    dialogSectioin.value.style.backgroundColor = "rgba(225,155,255,1)";
+    const id = setInterval(() => {
+      dialogSectioin.value.style.height =
+        parseFloat(window.getComputedStyle(dialogSectioin.value).height) + 10 + "px";
+      // console.log(dialogSectioin.value.style.height);
+    }, 1);
+    setTimeout(() => {
+      dialogSectioin.value.style.height = "calc(100vh - 20px)";
+      clearInterval(id);
+    }, 100);
+  } else if (!user.value.contains(event.target)) {
+    showLoginModal.value = false;
+    if (hide.value === true) return;
+    titleRefs.value.forEach((v, i) => {
+      v.style.borderRadius = "10px";
+      v.style.opacity = "0.3";
+    });
+    dialogSet.value.style.marginBottom = "0";
+
+    //设置向上收缩的动态效果
+    const id = setInterval(() => {
+      dialogSectioin.value.style.height =
+        parseFloat(window.getComputedStyle(dialogSectioin.value).height) - 18 + "px";
+      // console.log(dialogSectioin.value.style.height);
+    }, 1);
+    setTimeout(() => {
+      dialogSectioin.value.style.height = "auto";
+      clearInterval(id);
+      dialogSectioin.value.style.backgroundColor = "rgba(255,255,255,0)";
+      hide.value = true;
+    }, 100);
+  }
+};
+
+const dialogSet = ref(null);
+let canScroll = true;
+//设置对话合集滚动固定距离
+const handleScroll = (event) => {
+  event.preventDefault();
+
+  //避免多次调用，进而降低灵敏度，1s内只调用一次
+  if (!canScroll) return;
+  canScroll = false;
+  setTimeout(() => {
+    canScroll = true;
+  }, 1200);
+
+  const width = parseFloat(window.getComputedStyle(dialogSet.value).width);
+  if (event.deltaX > 0) {
+    var distance = Math.ceil(event.deltaX / width) * width;
+  } else {
+    var distance = Math.floor(event.deltaX / width) * width;
+  }
+  console.log(event.deltaX, "px");
+  setTimeout(() => {
+    dialogSet.value.scrollLeft -= event.deltaX;
+    dialogSet.value.scrollLeft += distance;
+    console.log(event.deltaX, distance, "px");
+  }, 1000);
+};
+
+// 编辑倾斜角度和旋转角度
 let scene = null;
 const pitch = ref(30);
 const rotation = ref(0);
@@ -146,7 +240,8 @@ onMounted(() => {
     }),
   });
 
-  fetch("https://gw.alipayobjects.com/os/rmsportal/oVTMqfzuuRFKiDwhPSFL.json")
+  // fetch("https://gw.alipayobjects.com/os/rmsportal/oVTMqfzuuRFKiDwhPSFL.json")
+  fetch("http://127.0.0.1:8000/property/getData")
     .then((res) => res.json())
     .then((data) => {
       const pointLayer = new PointLayer({})
@@ -159,7 +254,7 @@ onMounted(() => {
         })
         .shape("circle")
         .size("t", function (level) {
-          return [level * 1 + 10, level * 10 + 10];
+          return [level * 0.01 + 10, level * 0.1 + 10];
         })
         .color("t", [
           "#094D4A",
@@ -183,35 +278,35 @@ onMounted(() => {
       scene.addLayer(pointLayer);
     });
 
-  scene.on("loaded", () => {
-    fetch(
-      "https://gw.alipayobjects.com/os/basement_prod/d3564b06-670f-46ea-8edb-842f7010a7c6.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const layer = new HeatmapLayer({})
-          .source(data)
-          .shape("heatmap")
-          .size("mag", [0, 0.2]) // weight映射通道
-          .style({
-            intensity: 2,
-            radius: 20,
-            rampColors: {
-              colors: [
-                "#FF4818",
-                "#F7B74A",
-                "#FFF598",
-                "#91EABC",
-                "#2EA9A1",
-                "#206C7C",
-              ].reverse(),
-              positions: [0, 0.2, 0.4, 0.6, 0.8, 1.0],
-            },
-            zIndex: 0,
-          });
-        scene.addLayer(layer);
-      });
-  });
+  // scene.on("loaded", () => {
+  //   fetch(
+  //     "https://gw.alipayobjects.com/os/basement_prod/d3564b06-670f-46ea-8edb-842f7010a7c6.json"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const layer = new HeatmapLayer({})
+  //         .source(data)
+  //         .shape("heatmap")
+  //         .size("mag", [0, 0.2]) // weight映射通道
+  //         .style({
+  //           intensity: 2,
+  //           radius: 20,
+  //           rampColors: {
+  //             colors: [
+  //               "#FF4818",
+  //               "#F7B74A",
+  //               "#FFF598",
+  //               "#91EABC",
+  //               "#2EA9A1",
+  //               "#206C7C",
+  //             ].reverse(),
+  //             positions: [0, 0.2, 0.4, 0.6, 0.8, 1.0],
+  //           },
+  //           zIndex: 0,
+  //         });
+  //       scene.addLayer(layer);
+  //     });
+  // });
 });
 
 onMounted(() => {
@@ -225,89 +320,13 @@ onMounted(() => {
   }
 });
 
-// //点图层亮度图
-// onMounted(() => {
-//   const scene = new Scene({
-//     id: "map",
-//     map: new GaodeMap({
-//       center: [121.417463, 31.215175],
-//       style: "light",
-//       zoom: 11,
-//       pitch: pitch.value,
-//       rotation: rotation.value
-
-//     }),
-//   });
-//   scene.on("loaded", () => {
-//     fetch("https://gw.alipayobjects.com/os/rmsportal/BElVQFEFvpAKzddxFZxJ.txt")
-//       .then((res) => res.text())
-//       .then((data) => {
-//         const pointLayer = new PointLayer({})
-//           .source(data, {
-//             parser: {
-//               type: "csv",
-//               y: "lat",
-//               x: "lng",
-//             },
-//           })
-//           .size(0.5)
-//           .color("#080298");
-//         scene.addLayer(pointLayer);
-//       });
-//   });
-// });
-
-// //热力图层
-// onMounted(() => {
-//   const scene = new Scene({
-//     id: "map",
-//     map: new GaodeMap({
-//       style: "dark",
-//       center: [127.5671666579043, 7.445038892195569],
-//       zoom: 2.632456779444394,
-//     }),
-//   });
-//   scene.on("loaded", () => {
-//     fetch(
-//       "https://gw.alipayobjects.com/os/basement_prod/d3564b06-670f-46ea-8edb-842f7010a7c6.json"
-//     )
-//       .then((res) => res.json())
-//       .then((data) => {
-//         const layer = new HeatmapLayer({})
-//           .source(data)
-//           .shape("heatmap")
-//           .size("mag", [0, 1.0]) // weight映射通道
-//           .style({
-//             intensity: 2,
-//             radius: 20,
-//             rampColors: {
-//               colors: [
-//                 "#FF4818",
-//                 "#F7B74A",
-//                 "#FFF598",
-//                 "#91EABC",
-//                 "#2EA9A1",
-//                 "#206C7C",
-//               ].reverse(),
-//               positions: [0, 0.2, 0.4, 0.6, 0.8, 1.0],
-//             },
-//           });
-//         scene.addLayer(layer);
-//       });
-//   });
-// });
-
-// const input = ref("");
-// const inputBox = document.querySelector("#inputBox");
-// const textareaHeight = ref("auto");
-// const adjustHeight = () => {
-//   const textarea = $refs.inputBox;
-//   // 重置高度，防止其被其他样式影响
-//   textarea.style.height = "auto";
-//   // 根据内容设置高度
-//   textarea.style.height = `${textarea.scrollHeight}px`;
-// };
-onMounted(() => {});
+//登录注册模块
+const user = ref(null);
+const showLoginModal = ref(false);
+const toggleShowLoginModal = () => {
+  showLoginModal.value = !showLoginModal.value;
+  console.log(showLoginModal.value);
+};
 </script>
 
 <style scoped lang="scss">
@@ -323,87 +342,90 @@ onMounted(() => {});
 //   // overflow: hidden;
 //   overflow: hidden;
 
-  .mapStyle {
-    // justify-content: center;
-    // position: relative;
-    // flex: 1;
+.mapStyle {
+  // justify-content: center;
+  // position: relative;
+  // flex: 1;
+  width: 50%;
 
-    .setPitch {
-      position: absolute;
-      z-index: 10000;
-      right: 0;
-      bottom: 0;
+  .setPitch {
+    position: absolute;
+    z-index: 10000;
+    left: 0;
+    bottom: 40px;
 
-      .buttonStyle {
-        width: 30px;
-        height: 30px;
-        margin: 5px;
-      }
+    .buttonStyle {
+      width: 30px;
+      height: 30px;
+      margin: 5px;
     }
-    .setRotation {
-      position: absolute;
-      z-index: 10000;
-      left: 0;
-      bottom: 0;
+  }
+  .setRotation {
+    position: absolute;
+    z-index: 10000;
+    left: 0;
+    bottom: 0;
 
-      .buttonStyle {
-        width: 30px;
-        height: 30px;
-        margin: 5px;
-      }
+    .buttonStyle {
+      width: 30px;
+      height: 30px;
+      margin: 5px;
     }
   }
 
-  .dialogStyle {
-    background-color: beige;
-    width: 30%;
+  .userStyle {
+    position: absolute;
+    z-index: 10000;
+    left: 0;
+    top: 0;
+    background-color: aqua;
+    height: 30px;
+    width: 30px;
     padding: 10px;
-    position:fixed;
-    right:0;
-    z-index:10000;
+    border-radius: 10px;
+    margin: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 
-    .outputStyle {
-      background-color: rgb(213, 213, 88);
-      border-radius: 10px;
-      height: 100%;
-      box-sizing: border-box;
-      position: relative;
-      padding: 10px;
-      padding-top: 0;
-      line-height: 30px;
+.dialogSectioinStyle {
+  background-color: rgba(225, 155, 255, 1);
+  border-radius: 10px;
+  height: calc(100vh - 20px);
+  width: 30%;
+  box-sizing: border-box;
+  position: absolute;
+  padding-top: 0;
+  line-height: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  position: fixed;
+  right: 0;
+  z-index: 10000;
+
+  .dialogSetStyle {
+    width: 100%;
+    display: flex;
+    overflow: auto;
+    margin-bottom: 70px;
+    scroll-behavior: smooth;
+
+    .dialogStyle {
       display: flex;
       flex-direction: column;
+      width: 100%;
+      flex-shrink: 0;
       align-items: center;
-
-      .inputStyle {
-        position: absolute;
-        background-color: greenyellow;
-        bottom: 0;
-        left: 0;
-        margin: 10px;
-        padding: 10px;
-        border-radius: 10px;
-        width: 90%;
-        box-sizing: border-box;
-        max-height: 300px;
-        min-height: 60px;
-        max-width: 90%;
-        min-width: 90%;
-      }
-
-      .saveButtonStyle {
-        background-color: cyan;
-        border-radius: 10px;
-        padding: 5px 10px;
-      }
-      .saveButtonStyle:hover {
-        cursor: pointer;
-      }
 
       .titleStyle {
         background-color: rgb(250, 142, 130);
         width: 100%;
         padding: 10px;
+        box-sizing: border-box;
         border-radius: 10px 10px 0 0;
       }
       .contentStyle {
@@ -411,9 +433,45 @@ onMounted(() => {});
         width: 100%;
         pointer-events: none;
         padding: 10px;
+        box-sizing: border-box;
         border-radius: 0 0 10px 10px;
+      }
+      .saveButtonStyle {
+        background-color: cyan;
+        border-radius: 10px;
+        padding: 5px 10px;
+        margin: 10px auto;
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .saveButtonStyle:hover {
+        cursor: pointer;
       }
     }
   }
+
+  .inputStyle {
+    position: absolute;
+    background-color: greenyellow;
+    bottom: 0;
+    left: 0;
+    margin: 10px;
+    padding: 10px;
+    border-radius: 10px;
+    width: calc(100% - 20px);
+    box-sizing: border-box;
+    max-height: 300px;
+    min-height: 60px;
+    max-width: calc(100% - 20px);
+    min-width: 90%;
+  }
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+
 // }
 </style>
